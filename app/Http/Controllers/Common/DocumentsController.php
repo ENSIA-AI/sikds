9330970ef8cbc39a4f3d505ab2559e15259f1881
Yassir CHERDOUH <yassir.cherdouh13@gmail.com>
2026-04-18 08:54:11 +0100

    restore publish feat that was removed by someones commit

--- app/Http/Controllers/Common/DocumentsController.php
+++ app/Http/Controllers/Common/DocumentsController.php
@@ -199,7 +199,10 @@ class DocumentsController
             ])->values()->all()
             : $this->documentTags($document->id);
 
-        $actions = ['download', 'copy'];
+        $actions = ['download'];
+        if ($document->status === 'draft' && $user->can('document.publish')) {
+            $actions[] = 'publish';
+        }
         if ($this->canPreview($user)) {
             $actions[] = 'view';
         }
@@ -229,6 +232,7 @@ class DocumentsController
             'edit_url' => route('documents.edit', $document->id),
             'delete_url' => route('api.documents.destroy', $document->id),
             'restore_url' => route('api.documents.restore', $document->id),
+            'publish_url' => route('api.documents.publish', $document->id),
         ];
     }
 
